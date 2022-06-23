79e2fe559a6bc60a623ea436875b410dbc5b2057
Sebax00o <81639550+Sebax00o@users.noreply.github.com>
2022-06-23 16:06:51 -0400

    comentando polling

--- polling.js
+++ polling.js
@@ -2,18 +2,18 @@
 const app = require('./app');
 const axios = require("axios");
 
-// Set database connection credentials
+// // Set database connection credentials
 
-// Create pool
-const pool = axios.get('https://www.buda.com/api/v2/markets/eth-btc/ticker')
+// // Create pool
+// const pool = axios.get('https://www.buda.com/api/v2/markets/eth-btc/ticker')
 
-//keep alive every 15s
-setInterval(function () {
-    pool.catch((error) => {
-        console.warn('Not good man :(Get markets)'+error.message);
-    });
-}, 150000);
+// //keep alive every 15s
+// setInterval(function () {
+//     pool.catch((error) => {
+//         console.warn('Not good man :(Get markets)'+error.message);
+//     });
+// }, 150000);
 
-// Export the pool
-module.exports = pool;
+// // Export the pool
+// module.exports = pool;
 
