220111f24bc1214dc4552abb0c13905667693ee2
Sebax00o <81639550+Sebax00o@users.noreply.github.com>
2022-06-23 15:21:33 -0400

    error 503 axios

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000;
 const axios = require("axios");
+const pool = require('./polling');
 //const main = require('./index');
 
 app.get('/api', async (req,res)=> {
@@ -10,50 +11,29 @@ app.get('/api', async (req,res)=> {
     //   de cualquiera de los mercados de Buda.com.
     array = [];
     // http request con axios hacia la api para obtener datos
-    let response = await axios.get('https://www.buda.com/api/v2/markets/eth-btc/ticker')
-    .catch(function (error) {
-      if (error.response) {
-        // Request made and server responded
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-  
-    });
+    // let response =  axios.get('https://www.buda.com/api/v2/markets/eth-btc/ticker');
     
     //separamos los datos obtenidos en la consulta http, en este caso el indice se llama "ticker"
-    let {
-        data: {ticker},
-    } = response;
+    // let {
+    //     data: {ticker},
+    // } = response;
     //obtenemos los datos que nos importan de ticker
     //calculo del spread usando los datos obtenidos
-    let spread = (ticker.max_bid[0] - ticker.min_ask[0]);
+    // let spread = (ticker.max_bid[0] - ticker.min_ask[0]);
     //console.log("\n market : " + ticker.market_id +"\n max bid: " + ticker.max_bid[0] + "\n min ask: " + ticker.min_ask[0] + "\n spread : " + spread);
     //2. Necesitamos obtener el spread de todos 
     //   los mercados en una sola llamada.
     //
     // http request con axios hacia la api para obtener datos
-    let GetMarkets = await axios.get("https://www.buda.com/api/v2/markets", {
-      headers: {
-        Accept: "application/json",
-        "User-Agent": "axios 0.21.1"
-      }}).catch((error) => {
-          console.warn('Not good man :(Get markets)'+error.message);
-      });
+    let GetMarkets = await axios.get("https://www.buda.com/api/v2/markets");
     //separamos los datos obtenidos en la consulta http, en este caso el indice se llama "markets"
-    let {data: { markets },} = GetMarkets;
+    let {data: {markets},} = GetMarkets;
     //con .map filtramos cada dato en "markets", en este caso la "id" y se le asigna a la variable "market_id"
     var market_id = markets.map((markets) => markets.id);
     //ciclo for para hacer http get por cada dato hacia la segunda url 
     for(index = 0; index < market_id.length; index++){
             // http request con axios hacia la api para obtener datos
-            let GetSpread = await axios.get(`https://www.buda.com/api/v2/markets/${market_id[index]}/ticker`).catch((error) => {
+            let GetSpread =  await axios.get(`https://www.buda.com/api/v2/markets/${market_id[index]}/ticker`).catch((error) => {
                   console.warn('Not good man :('+error);
               });
             //separamos los datos obtenidos, en este caso el indice se llama "ticker"
@@ -64,8 +44,8 @@ app.get('/api', async (req,res)=> {
     //3. Necesitamos guardar un spread de “alerta” el cual en el futuro
     //   consultaremos por medio de polling si el spread es mayor o menor de ese spread. 
             // guardamos la alerta en una variable
-            
-            if (typeof localStorage === "undefined" || localStorage === null||localStorage.getItem(`alerta-${[index]}`) ==="ENOENT") {
+            function alert(index,spread) {
+              if (typeof localStorage === "undefined" || localStorage === null||localStorage.getItem(`alerta-${[index]}`) ==="ENOENT") {
                 var LocalStorage = require('node-localstorage').LocalStorage;
                 localStorage = new LocalStorage('./scratch');
                 localStorage.setItem(`alerta-${[index]}`, spread);
@@ -78,12 +58,15 @@ app.get('/api', async (req,res)=> {
               }
               // guardamos la alerta en una variable
             let alerta = localStorage.getItem(`alerta-${[index]}`) || 0 ;
+              return alerta;
+            }
+            alert(index, spread);
             //si el spread actual es igual al spread maximo guardado, generamos una alerta en consola.
-            if (parseInt(spread)===parseInt(alerta)) {
-                console.log("Alert on: "+String(ticker.market_id)+"|max spread: "+alerta);
+            if (parseInt(spread)===parseInt(alert(index, spread))) {
+                console.log("Alert on: "+String(ticker.market_id)+"|max spread: "+alert(index, spread));
               }
             //creamos el objeto values con todos los datos que vamos a mostrar en la web  
-            let values = [{market: String(ticker.market_id), max_bid: ticker.max_bid[0], min_ask: ticker.min_ask[0], spread: spread, alerta: alerta}];
+            let values = [{market: String(ticker.market_id), max_bid: ticker.max_bid[0], min_ask: ticker.min_ask[0], spread: spread, alerta: alert(index, spread)}];
             //guardamos los valores dentro de un arreglo
             array.push(values);
         }
@@ -91,16 +74,22 @@ app.get('/api', async (req,res)=> {
         //se envia como respuesta un json con todos los valores dentro del arreglo
         res.json(array)
         
+        
     
   } catch (error) {
     console.warn(error);
     //setResults([]);
     
   }
-    
+
         
 });
-
+app.get('/',(req, res) =>{   
+  const error = req.error;
+  if (error) throw error;// not connected!
+  console.log('Connected as IP: ' + req.ip +'\n');
+  res.send('Connected as IP: ' + req.ip +'\n'+'Please use /api');
+});
 app.listen(port, (error) =>{
     if(!error){
       console.log("Server is Successfully Running, \n"+ "and App is listening on: \n"+`http://localhost:${port}\n`)
---
+++ polling.js
@@ -0,0 +1,23 @@
+//Loads modules
+const app = require('./app');
+const axios = require("axios");
+
+// Set database connection credentials
+
+// Create pool
+const pool = axios.get('https://www.buda.com/api/v2/markets/eth-btc/ticker', {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "axios 0.21.1"
+    }})
+
+//keep alive every 15s
+setInterval(function () {
+    pool.catch((error) => {
+        console.warn('Not good man :(Get markets)'+error.message);
+    });
+}, 150000);
+
+// Export the pool
+module.exports = pool;
+
